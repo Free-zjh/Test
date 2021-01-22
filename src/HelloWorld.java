a66d553c99b4704103707b4bafde737e9ebb7bc3
lenovo <1461686619@qq.com>
2021-01-22 11:45:19 +0800

    test

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * @author shkstart
+ * @create 2021-01-21 21:33
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+
+}
